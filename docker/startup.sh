f8d8047e5566201380da0b80a071d6e216d4bd26
Dan Bedard <danb@renci.org>
2014-12-17 16:55:07 -0500

    Adding kibana4 beta to the mix

--- docker/startup.sh
+++ docker/startup.sh
@@ -1,15 +1,19 @@
 docker build -t termite/elasticsearch ./elasticsearch
 docker build -t termite/kibana ./kibana
+docker build -t termite/kibana4 ./kibana4
 docker build -t termite/logstash ./logstash
 
 docker kill elasticsearch ; docker rm elasticsearch
 docker kill kibana ; docker rm kibana
+docker kill kibana4 ; docker rm kibana4
 docker kill logstash ; docker rm logstash
 
 docker run --name elasticsearch -d -p 9200:9200 -p 9300:9300 \
   -v /opt/elasticsearch/data:/opt/elasticsearch/data \
   --name elasticsearch termite/elasticsearch
 docker run --name kibana -d -p 80:80 termite/kibana
+docker run --name kibana4 --link elasticsearch:elasticsearch \
+ -d -p 5601:5601 termite/kibana4
 docker run --name logstash --link elasticsearch:elasticsearch \
   -d -p 1514:1514 -v /ftp_logs:/opt/logs \
   termite/logstash -f /etc/ls-ftplogs.conf
